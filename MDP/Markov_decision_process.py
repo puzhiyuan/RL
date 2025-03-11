a383bddf6512e6d3c9270c72086551098734eb12
puzhiyuan <puzhiyuan185489643@gmail.com>
2025-03-11 21:43:42 +0800

    first commit

---
+++ MDP/Markov_decision_process.py
@@ -0,0 +1,17 @@
+import numpy as np
+from utils import compute
+
+gamma = 0.5
+# 转化后的MRP的状态转移矩阵
+P_from_mdp_to_mrp = [
+    [0.5, 0.5, 0.0, 0.0, 0.0],
+    [0.5, 0.0, 0.5, 0.0, 0.0],
+    [0.0, 0.0, 0.0, 0.5, 0.5],
+    [0.0, 0.1, 0.2, 0.2, 0.5],
+    [0.0, 0.0, 0.0, 0.0, 1.0],
+]
+P_from_mdp_to_mrp = np.array(P_from_mdp_to_mrp)
+R_from_mdp_to_mrp = [-0.5, -1.5, -1.0, 5.5, 0]
+
+V = compute(P_from_mdp_to_mrp, R_from_mdp_to_mrp, gamma, 5)
+print("MDP中每个状态价值分别为\n", V)
---
+++ TD/utils.py
@@ -0,0 +1,16 @@
+ 
+# 打印最终策略
+def print_agent(agent, env, action_meaning, disaster=[], end=[]):
+    for i in range(env.nrow):
+        for j in range(env.ncol):
+            if (i * env.ncol + j) in disaster:
+                print('****', end=' ')
+            elif (i * env.ncol + j) in end:
+                print('EEEE', end=' ')
+            else:
+                a = agent.best_action(i * env.ncol + j)
+                pi_str = ''
+                for k in range(len(action_meaning)):
+                    pi_str += action_meaning[k] if a[k] > 0 else 'o'
+                print(pi_str, end=' ')
+        print()
